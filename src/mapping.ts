02653d4ecc4ccfb8b8330dab45bc0161950ce77f
Myroslav Gavryliak <myroslav.gavriliak@gmail.com>
2022-06-14 21:37:48 +0300

    catch buy event for near chain

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,34 +1,59 @@
-import { near, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
+import { near, JSONValue, json, ipfs, log, BigInt } from "@graphprotocol/graph-ts";
+import { NFTToken, User } from "../generated/schema";
 
-export function handleReceipt(
-  receiptWithOutcome: near.ReceiptWithOutcome
+export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
+  const timestamp: u64 = receipt.block.header.timestampNanosec;
+  const actions = receipt.receipt.actions;
+  for (let i = 0; i < actions.length; i++) {
+    handleAction(actions[i], receipt.receipt, timestamp);
+  }
+}
+
+function handleAction(
+  action: near.ActionValue,
+  actionReceipt: near.ActionReceipt,
+  timestamp: u64,
 ): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(receiptWithOutcome.receipt.id.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(receiptWithOutcome.receipt.id.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (action.kind != near.ActionKind.FUNCTION_CALL) {
+    return;
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  const functionCall = action.toFunctionCall();
+  const methodName = functionCall.methodName;
 
-  // Entity fields can be set based on receipt information
-  entity.block = receiptWithOutcome.block.header.hash
+  if(methodName == "mint_nft") {
+    log.info('this is mint_nft triggered', []);
+  }
+  
+  if (methodName == "buy") {
+    const buyer = actionReceipt.signerId;
+    log.info('buyer: {}', [buyer]);
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    const args = json.try_fromBytes(functionCall.args).value;
+    const tokenId = args.toObject().get("token_id");
+    if (tokenId) {
+      log.info("arguments: {}", [tokenId.toString()])
+      let token = NFTToken.load(tokenId.toString());
+      if (token) {
+        token.owner = buyer.toString();
+        token.last_change = BigInt.fromU64(timestamp);
+      } else {
+        // create new token with new owner (when buy happened)
+        token = new NFTToken(tokenId.toString());
+        token.owner = buyer.toString();
+        token.last_change = BigInt.fromU64(timestamp);
+      }
+      token.save();
+    
+      let user = User.load(buyer);
+      if (!user) {
+        user = new User(buyer);
+        user.save();
+      }
+    } else {
+      log.info("no token id", []);
+    }
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  }
+
 }
